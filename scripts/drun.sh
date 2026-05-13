69abc3895ce0ef991ae3e7b059b3b5af6f20404f
Joey574 <joeysoroka@outlook.com>
2026-05-12 20:15:01 -0700

    secure the drun.sh script

--- scripts/drun.sh
+++ scripts/drun.sh
@@ -3,7 +3,12 @@ sudo apparmor_parser -r -W topography-apparmor-profile.txt
 
 docker run \
     --rm \
+    --read-only \
+    --name topo \
     -p 8080:8080 \
+    --cap-drop=ALL \
+    --tmpfs /tmp:rw,noexec,nosuid,size=64m \
+    --security-opt=no-new-privileges:true \
     --security-opt seccomp=seccomp.json \
     --security-opt apparmor=topography-apparmor-profile \
     topography:slim
