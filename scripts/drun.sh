042d1dc85e75f04da52200e8ef3d03b98dbd84cd
Joey574 <joeysoroka@outlook.com>
2026-05-12 19:35:18 -0700

    cleanup security stuff

--- scripts/drun.sh
+++ scripts/drun.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apparmor_parser -r -W topography-apparmor-profile.txt
 
-# --security-opt apparmor=topography-apparmor-profile \
 docker run \
     --rm \
     -p 8080:8080 \
--- scripts/slim_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-slim build \
-  --target topography:latest \
-  --tag topography:slim \
-  --expose 8080 \
-  --publish-port 8080:8080 \
-  --http-probe-ports 8080 \
-  --show-clogs \
-  --copy-meta-artifacts .
